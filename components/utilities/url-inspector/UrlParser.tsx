e53d5fc138da229328123c97642e8046a7d9c376
zero-route <iostream911@gmail.com>
2026-09-13 18:15:25 +0800

    add components/utilites/url-inspector/UrlParser.tsx

---
+++ components/utilities/url-inspector/UrlParser.tsx
@@ -0,0 +1,7 @@
+export default function UrlParser() {
+  return (
+    <div className="rounded-xl border border-border bg-surface p-5 text-sm text-textMuted">
+      URL parser belum diimplementasikan
+    </div>
+  )
+}
