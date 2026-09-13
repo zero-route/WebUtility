02398fa9473fde815e56981ebc0d0430a63f60f2
zero-route <iostream911@gmail.com>
2026-09-13 17:48:35 +0800

    add components/instagram/InstagramDownloader.tsx

---
+++ components/downloaders/instagram/InstagramDownloader.tsx
@@ -0,0 +1,7 @@
+export default function InstagramDownloader() {
+  return (
+    <div className="rounded-xl border border-border bg-surface p-5 text-sm text-textMuted">
+      Instagram downloader belum diimplementasikan
+    </div>
+  )
+}
