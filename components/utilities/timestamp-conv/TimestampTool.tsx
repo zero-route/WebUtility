533335c4d56113220d52378613aad321b795d1d5
zero-route <iostream911@gmail.com>
2026-09-13 18:13:58 +0800

    add components/utilities/timestamp-conv/TimestampTool.tsx

---
+++ components/utilities/timestamp-conv/TimestampTool.tsx
@@ -0,0 +1,7 @@
+export default function TimestampTool() {
+  return (
+    <div className="rounded-xl border border-border bg-surface p-5 text-sm text-textMuted">
+      Timestamp converter belum diimplementasikan
+    </div>
+  )
+}
