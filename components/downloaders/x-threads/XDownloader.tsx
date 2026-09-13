f9da8d323b4fa944a0c8024cac04ee3be666ced6
zero-route <iostream911@gmail.com>
2026-09-13 17:50:42 +0800

    add components/x-threads/XDownloader.tsx

---
+++ components/downloaders/x-threads/XDownloader.tsx
@@ -0,0 +1,7 @@
+export default function XDownloader() {
+  return (
+    <div className="rounded-xl border border-border bg-surface p-5 text-sm text-textMuted">
+      X and Threads downloader belum diimplementasikan
+    </div>
+  )
+}
