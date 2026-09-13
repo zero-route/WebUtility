e3a572fbc39a20b3630a8c187c608adfbb9de322
zero-route <iostream911@gmail.com>
2026-09-13 18:12:21 +0800

    add components/utilities/subnet-calc/SubnetCalc.tsx

---
+++ components/utilities/subnet-calc/SubnetCalc.tsx
@@ -0,0 +1,7 @@
+export default function SubnetCalc() {
+  return (
+    <div className="rounded-xl border border-border bg-surface p-5 text-sm text-textMuted">
+      Subnet calculator belum diimplementasikan
+    </div>
+  )
+}
