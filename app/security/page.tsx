895a0ac83ea687480b73ef40e4dab05a6b8bc960
zero-route <iostream911@gmail.com>
2026-09-14 18:46:52 +0800

    update page.tsx

--- app/security/page.tsx
+++ app/security/page.tsx
@@ -1,4 +1,5 @@
 import Base64EncodeDecodeUI from '@/components/security/base64-encode-decode/Base64EncodeDecodeUI'
+import EncryptorUI from '@/components/security/text-encryptor/EncryptorUI'
 import ToolPlaceholderCard from '@/components/dashboard/ToolPlaceholderCard'
 
 export default function SecurityPage() {
@@ -7,11 +8,9 @@ export default function SecurityPage() {
       <h1 className="text-center font-display text-2xl font-medium text-textPrimary">Security tools</h1>
       <div className="mt-10 grid grid-cols-1 gap-4 lg:grid-cols-2">
         <Base64EncodeDecodeUI />
-        <div className="flex flex-col gap-4">
-          <ToolPlaceholderCard name="AES encryptor" index={0} />
-          <ToolPlaceholderCard name="Password generator" index={1} />
-          <ToolPlaceholderCard name="Hash generator" index={2} />
-        </div>
+        <EncryptorUI />
+        <ToolPlaceholderCard name="Password generator" index={0} />
+        <ToolPlaceholderCard name="Hash generator" index={1} />
       </div>
     </div>
   )
