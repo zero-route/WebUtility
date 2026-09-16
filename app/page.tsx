c346c5b69dab1b04f4ba66ddf32f3a2e89959a46
zero-route <iostream911@gmail.com>
2026-09-16 18:21:25 +0800

    update page.tsx pancingan 2

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import WeatherCard from '@/components/dashboard/WeatherCard'
 import { categories } from '@/lib/toolsData'
 
 export const metadata: Metadata = {
-  title: 'Ringkasan tool'
+  title: 'Ringkasan tools'
 }
 
 export default function HomePage() {
